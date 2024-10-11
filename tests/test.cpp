e8224fc2acdd7df606df1d2f29e3add73aad62c4
璀境石 (ChuiKingShek) <git@chuikingshek.com>
2024-10-11 15:50:40 +0800

    test

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,6 +3,9 @@
 #include "win32/abi.hpp"
 
 int main() {
+	std::u8string_view hello(u8"你好，中国。");
+	std::wstring wide_hello(win32::to_wstring(hello));
+
 	uint32_t const dpi = win32::getDpiForSystem();
 	win32::Rect rc{ 0, 0, 640, 480 };
 	win32::adjustWindowRectExForDpi(rc, WS_OVERLAPPEDWINDOW, false, 0, dpi);
