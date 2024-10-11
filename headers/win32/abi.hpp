b014c07361f2329109bcbb9f4c668160e920c144
璀境石 (ChuiKingShek) <git@chuikingshek.com>
2024-10-11 17:34:09 +0800

    interpo

--- headers/win32/abi.hpp
+++ headers/win32/abi.hpp
@@ -20,21 +20,23 @@ namespace win32::abi {
 	static_assert(sizeof(::RECT) == sizeof(Rect));
 	template<> inline ::RECT* as(Rect* value) { return reinterpret_cast<::RECT*>(value); }
 	template<> inline Rect* as(::RECT* value) { return reinterpret_cast<Rect*>(value); }
+}
 
+namespace win32 {
 	inline bool adjustWindowRectExForDpi(::RECT* rect, uint32_t style, bool menu, uint32_t style_ex, uint32_t dpi) {
-		return adjustWindowRectExForDpi(as<Rect*>(rect), style, menu, style_ex, dpi);
+		return adjustWindowRectExForDpi(abi::as<Rect*>(rect), style, menu, style_ex, dpi);
 	}
 
 	// similar to getDpiForWindow, return 0 if fail
 	inline uint32_t getDpiForMonitor(::HMONITOR monitor) {
-		return getDpiForMonitor(as<MonitorHandle*>(monitor));
+		return getDpiForMonitor(abi::as<MonitorHandle*>(monitor));
 	}
 
 	inline uint32_t getDpiForWindow(::HWND window) {
-		return getDpiForWindow(as<WindowHandle*>(window));
+		return getDpiForWindow(abi::as<WindowHandle*>(window));
 	}
 
 	bool enableNonClientDpiScaling(::HWND window) {
-		return enableNonClientDpiScaling(as<WindowHandle*>(window));
+		return enableNonClientDpiScaling(abi::as<WindowHandle*>(window));
 	}
 }
